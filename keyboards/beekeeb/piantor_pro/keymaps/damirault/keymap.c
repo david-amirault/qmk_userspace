0913fa29a0fc7c6f6116b47375e48bd8f580d885
david-amirault <david-amirault@users.noreply.github.com>
2026-09-09 15:07:17 -0400

    use one shot layers and modifiers in keymap.c

--- keyboards/beekeeb/piantor_pro/keymaps/damirault/keymap.c
+++ keyboards/beekeeb/piantor_pro/keymaps/damirault/keymap.c
@@ -36,76 +36,76 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,    KC_Q,       KC_L,       KC_Y,       KC_G,       KC_K,                               KC_B,       KC_F,       KC_O,       KC_U,       KC_J,       _______,
         _______,    KC_C,       KC_R,       KC_S,       KC_T,       KC_M,                               MAGIC,      KC_N,       KC_A,       KC_E,       KC_I,       _______,
         _______,    KC_Z,       KC_X,       KC_V,       KC_D,       KC_W,                               KC_P,       KC_H,       KC_COMM,    KC_DOT,     KC_SLSH,    _______,
-                                                        KC_TAB,     KC_SPC,     MO(LHS),    MO(RHS),    KC_BSPC,    KC_ENT
+                                                        KC_TAB,     KC_SPC,     OSL(LHS),  OSL(RHS),    KC_BSPC,    KC_ENT
     ),
 
     [LHS] = LAYOUT_split_3x6_3(
         _______,    XXXXXXX,    KC_LCTL,    KC_LALT,    KC_LGUI,    XXXXXXX,                            S(KC_B),    S(KC_F),    S(KC_O),    S(KC_U),    S(KC_J),    _______,
-        _______,    S(KC_C),    MO(NUM),    MO(NMS),    MO(CMD),    MO(CDS),                            MAGIC,      S(KC_N),    S(KC_A),    S(KC_E),    S(KC_I),    _______,
-        _______,    XXXXXXX,    KC_LCTL,    KC_LALT,    KC_LGUI,    XXXXXXX,                            S(KC_P),    S(KC_H),    S(KC_COMM), S(KC_DOT),  S(KC_SLSH), _______,
+        _______,    S(KC_C),    OSL(NUM),   OSL(NMS),   OSL(CMD),   OSL(CDS),                           MAGIC,      S(KC_N),    S(KC_A),    S(KC_E),    S(KC_I),    _______,
+        _______,    XXXXXXX,    OS_LCTL,    OS_LALT,    OS_LGUI,    XXXXXXX,                            S(KC_P),    S(KC_H),    S(KC_COMM), S(KC_DOT),  S(KC_SLSH), _______,
                                                         _______,    _______,    _______,    KC_ESC,     S(KC_BSPC), S(KC_ENT)
     ),
 
     [NUM] = LAYOUT_split_3x6_3(
         _______,    XXXXXXX,    KC_LCTL,    KC_LALT,    KC_LGUI,    XXXXXXX,                            XXXXXXX,    KC_8,       KC_9,       KC_0,       XXXXXXX,    _______,
-        _______,    XXXXXXX,    _______,    MO(NMS),    MO(CMD),    MO(CDS),                            KC_QUOT,    KC_1,       KC_2,       KC_3,       KC_4,       _______,
-        _______,    XXXXXXX,    KC_LCTL,    KC_LALT,    KC_LGUI,    XXXXXXX,                            XXXXXXX,    KC_5,       KC_6,       KC_7,       KC_SLSH,    _______,
+        _______,    XXXXXXX,    OSL(NUM),   OSL(NMS),   OSL(CMD),   OSL(CDS),                           KC_QUOT,    KC_1,       KC_2,       KC_3,       KC_4,       _______,
+        _______,    XXXXXXX,    OS_LCTL,    OS_LALT,    OS_LGUI,    XXXXXXX,                            XXXXXXX,    KC_5,       KC_6,       KC_7,       KC_SLSH,    _______,
                                                         _______,    _______,    _______,    KC_ESC,     KC_BSPC,    KC_ENT
     ),
 
     [NMS] = LAYOUT_split_3x6_3(
         _______,    XXXXXXX,    KC_LCTL,    KC_LALT,    KC_LGUI,    XXXXXXX,                            XXXXXXX,    S(KC_8),    S(KC_9),    S(KC_0),    XXXXXXX,    _______,
-        _______,    XXXXXXX,    MO(NUM),    _______,    MO(CMD),    MO(CDS),                            S(KC_QUOT), S(KC_1),    S(KC_2),    S(KC_3),    S(KC_4),    _______,
-        _______,    XXXXXXX,    KC_LCTL,    KC_LALT,    KC_LGUI,    XXXXXXX,                            XXXXXXX,    S(KC_5),    S(KC_6),    S(KC_7),    S(KC_SLSH), _______,
+        _______,    XXXXXXX,    OSL(NUM),   OSL(NMS),   OSL(CMD),   OSL(CDS),                           S(KC_QUOT), S(KC_1),    S(KC_2),    S(KC_3),    S(KC_4),    _______,
+        _______,    XXXXXXX,    OS_LCTL,    OS_LALT,    OS_LGUI,    XXXXXXX,                            XXXXXXX,    S(KC_5),    S(KC_6),    S(KC_7),    S(KC_SLSH), _______,
                                                         _______,    _______,    _______,    S(KC_ESC),  S(KC_BSPC), S(KC_ENT)
     ),
 
     [CMD] = LAYOUT_split_3x6_3(
-        _______,    XXXXXXX,    KC_LCTL,    KC_LALT,    KC_LGUI,    QK_BOOT,                            XXXXXXX,    KC_SCLN,    KC_MINS,    KC_DOT,     XXXXXXX,    _______,
-        _______,    XXXXXXX,    MO(NUM),    MO(NMS),    _______,    MO(CDS),                            KM_NB,      KM_NF,      KC_VOLD,    KC_VOLU,    KC_MUTE,    _______,
-        _______,    XXXXXXX,    KC_LCTL,    KC_LALT,    KC_LGUI,    XXXXXXX,                            XXXXXXX,    KM_PH,      KC_BRID,    KC_BRIU,    XXXXXXX,    _______,
+        _______,    XXXXXXX,    KC_LCTL,    KC_LALT,    KC_LGUI,    XXXXXXX,                            XXXXXXX,    KC_SCLN,    KC_MINS,    KC_DOT,     XXXXXXX,    _______,
+        _______,    XXXXXXX,    OSL(NUM),   OSL(NMS),   OSL(CMD),   OSL(CDS),                           KM_NB,      KM_NF,      KC_VOLD,    KC_VOLU,    KC_MUTE,    _______,
+        _______,    XXXXXXX,    OS_LCTL,    OS_LALT,    OS_LGUI,    XXXXXXX,                            XXXXXXX,    KM_PH,      KC_BRID,    KC_BRIU,    XXXXXXX,    _______,
                                                         _______,    _______,    _______,    KC_ESC,     KC_BSPC,    KC_ENT
     ),
 
     [CDS] = LAYOUT_split_3x6_3(
-        _______,    XXXXXXX,    KC_LCTL,    KC_LALT,    KC_LGUI,    XXXXXXX,                            XXXXXXX,    S(KC_SCLN), S(KC_MINS), S(KC_DOT),  XXXXXXX,    _______,
-        _______,    XXXXXXX,    MO(NUM),    MO(NMS),    MO(CMD),    _______,                            KM_NB,      KM_NF,      S(KC_VOLD), S(KC_VOLU), S(KC_MUTE), _______,
-        _______,    XXXXXXX,    KC_LCTL,    KC_LALT,    KC_LGUI,    XXXXXXX,                            XXXXXXX,    KM_PH,      S(KC_BRID), S(KC_BRIU), XXXXXXX,    _______,
+        _______,    QK_BOOT,    KC_LCTL,    KC_LALT,    KC_LGUI,    XXXXXXX,                            XXXXXXX,    S(KC_SCLN), S(KC_MINS), S(KC_DOT),  XXXXXXX,    _______,
+        _______,    XXXXXXX,    OSL(NUM),   OSL(NMS),   OSL(CMD),   OSL(CDS),                           KM_NB,      KM_NF,      S(KC_VOLD), S(KC_VOLU), S(KC_MUTE), _______,
+        _______,    XXXXXXX,    OS_LCTL,    OS_LALT,    OS_LGUI,    XXXXXXX,                            XXXXXXX,    KM_PH,      S(KC_BRID), S(KC_BRIU), XXXXXXX,    _______,
                                                         _______,    _______,    _______,    S(KC_ESC),  S(KC_BSPC), S(KC_ENT)
     ),
 
     [RHS] = LAYOUT_split_3x6_3(
         _______,    S(KC_Q),    S(KC_L),    S(KC_Y),    S(KC_G),    S(KC_K),                            XXXXXXX,    KC_LGUI,    KC_LALT,    KC_LCTL,    XXXXXXX,    _______,
-        _______,    S(KC_C),    S(KC_R),    S(KC_S),    S(KC_T),    S(KC_M),                            MO(NVS),    MO(NAV),    MO(SMS),    MO(SYM),    S(KC_I),    _______,
-        _______,    S(KC_Z),    S(KC_X),    S(KC_V),    S(KC_D),    S(KC_W),                            XXXXXXX,    KC_LGUI,    KC_LALT,    KC_LCTL,    XXXXXXX,    _______,
+        _______,    S(KC_C),    S(KC_R),    S(KC_S),    S(KC_T),    S(KC_M),                            OSL(NVS),   OSL(NAV),   OSL(SMS),   OSL(SYM),   S(KC_I),    _______,
+        _______,    S(KC_Z),    S(KC_X),    S(KC_V),    S(KC_D),    S(KC_W),                            XXXXXXX,    OS_LGUI,    OS_LALT,    OS_LCTL,    XXXXXXX,    _______,
                                                         S(KC_TAB),  S(KC_SPC),  KC_ESC,     _______,    _______,    _______
     ),
 
     [SYM] = LAYOUT_split_3x6_3(
         _______,    XXXXXXX,    KC_LBRC,    KC_RBRC,    KC_QUOT,    XXXXXXX,                            XXXXXXX,    KC_LGUI,    KC_LALT,    KC_LCTL,    XXXXXXX,    _______,
-        _______,    KC_BSLS,    KC_GRV,     KC_MINS,    KC_SCLN,    CAPSWRD,                            MO(NVS),    MO(NAV),    MO(SMS),    _______,    XXXXXXX,    _______,
-        _______,    XXXXXXX,    NOT_EQL,    EN_DASH,    KC_EQL,     XXXXXXX,                            XXXXXXX,    KC_LGUI,    KC_LALT,    KC_LCTL,    XXXXXXX,    _______,
+        _______,    KC_BSLS,    KC_GRV,     KC_MINS,    KC_SCLN,    CAPSWRD,                            OSL(NVS),   OSL(NAV),   OSL(SMS),   OSL(SYM),   XXXXXXX,    _______,
+        _______,    XXXXXXX,    NOT_EQL,    EN_DASH,    KC_EQL,     XXXXXXX,                            XXXXXXX,    OS_LGUI,    OS_LALT,    OS_LCTL,    XXXXXXX,    _______,
                                                         KC_TAB,     KC_SPC,     KC_ESC,     _______,    _______,    _______
     ),
 
     [SMS] = LAYOUT_split_3x6_3(
         _______,    XXXXXXX,    S(KC_LBRC), S(KC_RBRC), S(KC_QUOT), XXXXXXX,                            XXXXXXX,    KC_LGUI,    KC_LALT,    KC_LCTL,    XXXXXXX,    _______,
-        _______,    S(KC_BSLS), S(KC_GRV),  S(KC_MINS), S(KC_SCLN), KC_CAPS,                            MO(NVS),    MO(NAV),    _______,    MO(SYM),    XXXXXXX,    _______,
-        _______,    XXXXXXX,    S(NOT_EQL), S(EN_DASH), S(KC_EQL),  XXXXXXX,                            XXXXXXX,    KC_LGUI,    KC_LALT,    KC_LCTL,    XXXXXXX,    _______,
+        _______,    S(KC_BSLS), S(KC_GRV),  S(KC_MINS), S(KC_SCLN), KC_CAPS,                            OSL(NVS),   OSL(NAV),   OSL(SMS),   OSL(SYM),   XXXXXXX,    _______,
+        _______,    XXXXXXX,    S(NOT_EQL), S(EN_DASH), S(KC_EQL),  XXXXXXX,                            XXXXXXX,    OS_LGUI,    OS_LALT,    OS_LCTL,    XXXXXXX,    _______,
                                                         S(KC_TAB),  S(KC_SPC),  S(KC_ESC),  _______,    _______,    _______
     ),
 
     [NAV] = LAYOUT_split_3x6_3(
         _______,    XXXXXXX,    KC_HOME,    KC_END,     KC_Q,       XXXXXXX,                            XXXXXXX,    KC_LGUI,    KC_LALT,    KC_LCTL,    XXXXXXX,    _______,
-        _______,    KC_LEFT,    KC_UP,      KC_DOWN,    KC_RGHT,    KC_INS,                             MO(NVS),    _______,    MO(SMS),    MO(SYM),    XXXXXXX,    _______,
-        _______,    XXXXXXX,    KC_PGUP,    KC_PGDN,    KC_DEL,     XXXXXXX,                            XXXXXXX,    KC_LGUI,    KC_LALT,    KC_LCTL,    XXXXXXX,    _______,
+        _______,    KC_LEFT,    KC_UP,      KC_DOWN,    KC_RGHT,    KC_INS,                             OSL(NVS),   OSL(NAV),   OSL(SMS),   OSL(SYM),   XXXXXXX,    _______,
+        _______,    XXXXXXX,    KC_PGUP,    KC_PGDN,    KC_DEL,     XXXXXXX,                            XXXXXXX,    OS_LGUI,    OS_LALT,    OS_LCTL,    XXXXXXX,    _______,
                                                         KC_TAB,     KC_SPC,     KC_ESC,     _______,    _______,    _______
     ),
 
     [NVS] = LAYOUT_split_3x6_3(
         _______,    XXXXXXX,    S(KC_HOME), S(KC_END),  S(KC_Q),    XXXXXXX,                            XXXXXXX,    KC_LGUI,    KC_LALT,    KC_LCTL,    XXXXXXX,    _______,
-        _______,    S(KC_LEFT), S(KC_UP),   S(KC_DOWN), S(KC_RGHT), S(KC_INS),                          _______,    MO(NAV),    MO(SMS),    MO(SYM),    XXXXXXX,    _______,
-        _______,    XXXXXXX,    S(KC_PGUP), S(KC_PGDN), S(KC_DEL),  XXXXXXX,                            XXXXXXX,    KC_LGUI,    KC_LALT,    KC_LCTL,    XXXXXXX,    _______,
+        _______,    S(KC_LEFT), S(KC_UP),   S(KC_DOWN), S(KC_RGHT), S(KC_INS),                          OSL(NVS),   OSL(NAV),   OSL(SMS),   OSL(SYM),   XXXXXXX,    _______,
+        _______,    XXXXXXX,    S(KC_PGUP), S(KC_PGDN), S(KC_DEL),  XXXXXXX,                            XXXXXXX,    OS_LGUI,    OS_LALT,    OS_LCTL,    XXXXXXX,    _______,
                                                         S(KC_TAB),  S(KC_SPC),  S(KC_ESC),  _______,    _______,    _______
     ),
 };
